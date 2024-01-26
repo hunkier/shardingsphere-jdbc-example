e2ce730344500178e242ce4416590dcf3b799f7e
hunkier <huangkuier@gmail.com>
2024-01-27 01:21:26 +0800

    deploy
    Signed-off-by: hunkier <huangkuier@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,23 @@ set -e
 
 # 生成静态文件
 #npm run docs:build
-#yarn docs:build
+yarn docs:build
+
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+ echo 'shardingsphere-jdbc-example.hunkier.cn' > CNAME
 
-#git init
+git init
 git add -A
 git commit -m 'deploy'
+
+
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
  git push -f git@github.com:hunkier/shardingsphere-jdbc-example.git master:gh-pages
-# git push  git@github.com:hunkier/shardingsphere-jdbc-example.git master:gh-pages
